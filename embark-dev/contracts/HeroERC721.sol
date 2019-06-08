419c26e194cc6d06e051829518e7275394a973c7
0x332 <kinspotting@protonmail.com>
2019-06-08 14:42:54 +0800

    add: hero name

--- embark-dev/contracts/HeroERC721.sol
+++ embark-dev/contracts/HeroERC721.sol
@@ -1,5 +1,13 @@
 pragma solidity ^0.4.23;
 
-contract HeroERC721 is ERC721 {
+import "./Ownable.sol";
+import "./ERC721.sol";
+
+
+contract HeroERC721 is ERC721, Ownable {
+
+  string public constant name = "Hero";
+  string public constant symbol = "HERO";
+
 
 }
