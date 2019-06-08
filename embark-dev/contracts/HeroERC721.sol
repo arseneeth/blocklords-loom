f65920e988f92884a1bcaf298f0c97530b8f33e8
0x332 <kinspotting@protonmail.com>
2019-06-08 15:32:00 +0800

    add: hero struct

--- embark-dev/contracts/HeroERC721.sol
+++ embark-dev/contracts/HeroERC721.sol
@@ -9,5 +9,10 @@ contract HeroERC721 is ERC721, Ownable {
   string public constant name = "Hero";
   string public constant symbol = "HERO";
 
+  struct Hero{
+  	uint id; 
+  }
 
+  Hero[] public heros;
+  
 }
