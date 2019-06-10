63f2197074e4539e5c1561ec36ff6c0fa991bb80
0x332 <kinspotting@protonmail.com>
2019-06-10 16:48:17 +0800

    wip: erc721 hero

--- embark-dev/contracts/Blocklords.sol
+++ embark-dev/contracts/Blocklords.sol
@@ -1,11 +1,33 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-import "./MetaDataStore.sol";
+import "./MetadataStore.sol";
+import "./HeroToken.sol";
+
+/**
+* @title Blocklords
+* @author Arseny Kin
+* @notice Blocklords main contract
+*/
 
 
-contract Blocklords is MetaDataStore{
+contract Blocklords is MetadataStore, HeroToken{
 
-  string public constant blocklords = "BLOCKLORDS";
+  // Market Durations
+  uint constant HOURS_8       = 28800;      // 28_800 Seconds are 8 hours
+  uint constant HOURS_12      = 43200;     // 43_200 Seconds are 12 hours
+  uint constant HOURS_24      = 86400;     // 86_400 Seconds are 24 hours
+
+  // Battle Results
+  uint constant ATTACKER_WON  = 1;
+  uint constant ATTACKER_LOSE = 2;
+
+  // Battle Types
+  uint constant PVP= 1;       // Player Against Player at the Strongholds
+  uint constant PVC= 2;       // Player Against City
+  uint constant PVE= 3;       // Player Against NPC on the map
+
+  MetadataStore public metadataStore;
+  HeroToken public heroToken;
 
 
 }
--- embark-dev/contracts/HeroToken.sol
+++ embark-dev/contracts/HeroToken.sol
@@ -2,9 +2,16 @@ pragma solidity >=0.5.0 <0.6.0;
 
 import "./common/Ownable.sol";
 import "./common/ERC721.sol";
+import "./MetadataStore.sol";
+
+/**
+* @title HeroToken
+* @author Arseny Kin
+* @notice Contract for ERC721 Hero token
+*/
 
 
-contract HeroERC721 is ERC721, Ownable {
+contract HeroToken is ERC721, Ownable {
 
   string public constant name = "Hero";
   string public constant symbol = "HERO";
@@ -14,5 +21,9 @@ contract HeroERC721 is ERC721, Ownable {
   }
 
   Hero[] public heros;
+
+  function mint() public onlyOwner{
+
+  }
 
 }
